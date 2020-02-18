869adbd1d5387772711015502f75394d7c04ee5d
Daniel Snouck <dsnouck@gmail.com>
2020-02-18 16:27:25 +0100

    Add MessagesControllerIntegrationTests.

---
+++ BackEnd/HelloWorld.WebApiIntegrationTests/Controllers/MessagesControllerIntegrationTests.cs
@@ -0,0 +1,53 @@
+﻿// <copyright file="MessagesControllerIntegrationTests.cs" company="dsnouck">
+// Copyright (c) dsnouck. All rights reserved.
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace HelloWorld.WebApiIntegrationTests.Controllers
+{
+    using System;
+    using System.Threading.Tasks;
+    using FluentAssertions;
+    using HelloWorld.WebApi;
+    using HelloWorld.WebApi.Controllers;
+    using Xunit;
+
+    /// <summary>
+    /// Provides integration tests for <see cref="MessagesController"/>.
+    /// </summary>
+    public class MessagesControllerIntegrationTests : IClassFixture<InMemoryWebApplicationFactory<Startup>>
+    {
+        private readonly InMemoryWebApplicationFactory<Startup> factory;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MessagesControllerIntegrationTests"/> class.
+        /// </summary>
+        /// <param name="factory">An <see cref="InMemoryWebApplicationFactory{TStartup}"/>.</param>
+        public MessagesControllerIntegrationTests(
+            InMemoryWebApplicationFactory<Startup> factory)
+        {
+            this.factory = factory;
+        }
+
+        /// <summary>
+        /// Tests GET /messages.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+        [Fact]
+        public async Task GivenMessagesWhenGetMessagesIsCalledThenAllMessagesAreReturned()
+        {
+            // Arrange
+            var client = this.factory.CreateClient();
+            var uri = new Uri("http://localhost/messages");
+
+            // Act
+            var response = await client.GetAsync(uri).ConfigureAwait(false);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            response.Content.Headers.ContentType.ToString().Should().Be("application/json; charset=utf-8");
+            var content = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+            content.Should().Be(@"[{""id"":1,""content"":""Hello, world!""}]");
+        }
+    }
+}
---
+++ BackEnd/HelloWorld.WebApiIntegrationTests/InMemoryWebApplicationFactory.cs
@@ -0,0 +1,54 @@
+﻿// <copyright file="InMemoryWebApplicationFactory.cs" company="dsnouck">
+// Copyright (c) dsnouck. All rights reserved.
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace HelloWorld.WebApiIntegrationTests
+{
+    using System;
+    using System.Linq;
+    using HelloWorld.Database;
+    using HelloWorld.Entities;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Mvc.Testing;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.DependencyInjection;
+
+    /// <summary>
+    /// A custom <see cref="WebApplicationFactory{TEntryPoint}"/> that uses an in memory database.
+    /// </summary>
+    /// <typeparam name="TStartup">The type of startup class.</typeparam>
+    public class InMemoryWebApplicationFactory<TStartup> : WebApplicationFactory<TStartup>
+        where TStartup : class
+    {
+        /// <inheritdoc/>
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            builder.ConfigureServices(serviceCollection =>
+            {
+                var serviceDescriptor = serviceCollection
+                    .SingleOrDefault(serviceDescriptor => serviceDescriptor.ServiceType == typeof(DbContextOptions<HelloWorldContext>));
+                if (serviceDescriptor != null)
+                {
+                    serviceCollection.Remove(serviceDescriptor);
+                }
+
+                serviceCollection.AddDbContext<HelloWorldContext>(options =>
+                    options.UseInMemoryDatabase("HelloWorld"));
+
+                var serviceProvider = serviceCollection.BuildServiceProvider();
+                using var scope = serviceProvider.CreateScope();
+                var scopedServiceProvider = scope.ServiceProvider;
+                var helloWorldContext = scopedServiceProvider.GetRequiredService<HelloWorldContext>();
+                helloWorldContext.Database.EnsureCreated();
+                helloWorldContext.Messages?.Add(new Message { Content = "Hello, world!" });
+                helloWorldContext.SaveChanges();
+            });
+        }
+    }
+}
