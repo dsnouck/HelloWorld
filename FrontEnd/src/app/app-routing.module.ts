e242ea6ba287b6ec9992478b99d7af7a1d5e19c5
Daniel Snouck <dsnouck@gmail.com>
2020-09-30 20:25:45 +0200

    Add MessageComponent.

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MessageComponent } from './components/message/message.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '**', component: MessageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
